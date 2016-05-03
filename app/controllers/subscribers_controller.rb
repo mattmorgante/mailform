1f0986a937cadce50c93e3c2e9a30c94b63cbd3d
Matt <matthewmorgante@gmail.com>
2016-05-02 20:08:41 -0400

    view and controllers setup

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,4 +1,17 @@
 class SubscribersController < ApplicationController
-  def index
+  def create
+    @subscriber = Subscriber.new(subscriber_params)
+    if @subscriber.save
+      cookies[:saved_lead] = true
+      redirect_to root_path, notice: "Saved Successfully!"
+    else 
+      redirect_to root_path, notice: "Failed to save"
+    end 
   end
+
+  private 
+
+    def subscriber_params
+      params.require(:lead).permit(:name, :email, :source)
+    end
 end
